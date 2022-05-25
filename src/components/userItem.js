aa82c2cf38cf2bd6228089fafae3a8346034c732
lan mi <miranda@landeMacBook-Pro.local>
2022-05-24 22:13:55 -0400

    tooltip update

--- src/components/userItem.js
+++ src/components/userItem.js
@@ -9,17 +9,28 @@ import {
   Paper,
   // Link,
 } from "@mui/material";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import { setUser } from "../store/userActions";
 import { useDispatch } from "react-redux";
+import { useState } from "react";
+import Tooltip from "@mui/material/Tooltip";
 
 const UserItem = (props) => {
-
   const dispatch = useDispatch();
+  const [hover, setHover] = useState(false);
+
+  const onHover = () => {
+    setHover(true);
+  };
+
+  const onLeave = () => {
+    setHover(false);
+  };
+
   return (
     <TableContainer
       component={Paper}
-      sx={{ maxHeight: "400px", maxWidth: "700px" }}
+      sx={{ maxHeight: "400px", maxWidth: "600px" }}
     >
       <Table aria-label="simple table" stickyHeader>
         <TableHead>
@@ -38,13 +49,20 @@ const UserItem = (props) => {
             >
               <TableCell>
                 {/* <Link href={`/profile/${u.id}`} onClick={() => {setUser(u)}}> */}
-                <Link to={`/profile/${u.id}`} onClick={() => dispatch(setUser(u))}>
+                <Link
+                  to={`/profile/${u.id}`}
+                  onClick={() => dispatch(setUser(u))}
+                >
                   {u.fname}
                   {u.lname}
                 </Link>
               </TableCell>
               <TableCell>{u.dob}</TableCell>
-              <TableCell>{u.email}</TableCell>
+              <TableCell>
+                <Tooltip title={u.email} placement="top" >
+                <span className="table-cell-trucate">{u.email}</span>
+                </Tooltip>
+              </TableCell>
               <TableCell>{u.gender}</TableCell>
             </TableRow>
           ))}

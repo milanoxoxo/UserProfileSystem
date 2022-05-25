0ce7799e623177b25b3131771bd1b211a96491ac
milanoxoxo <104524866+milanoxoxo@users.noreply.github.com>
2022-05-25 15:25:43 -0400

    Update userProfileReducers.js

--- src/store/userProfileReducers.js
+++ src/store/userProfileReducers.js
@@ -1,7 +1,6 @@
 function usersProfileReducer(state='', action){
   switch (action.type) {
     case 'SET_USER':
-      console.log(action.payload)
       return action.payload;
     default:
       return state;
